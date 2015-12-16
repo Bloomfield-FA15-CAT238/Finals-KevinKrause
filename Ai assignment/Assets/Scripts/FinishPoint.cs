4a26682ca4ec25a9f9f4cf684ca85494ac71b9a1
Kevin Krause <kevin_krause@bloomfield.edu>
2015-12-16 11:39:15 -0500

    Final - KevinKrause

---
+++ Ai assignment/Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FinishPoint : MonoBehaviour {
+	Text GameOverText;
+	public Text hud2;
+
+	// Use this for initialization
+	void Start () {
+		GameOverText = GameObject.Find ("GameOverText").GetComponent<Text> ();
+		GameOverText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter() {
+		GameOverText.enabled = true;
+	}
+}
---
+++ Ai assignment/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public bool isGame_Start = false;
+	
+	void OnMouseUp (){
+		if(isGame_Start){
+			Application.LoadLevel(1);
+		}
+	}
+}
